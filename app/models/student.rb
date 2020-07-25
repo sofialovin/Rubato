caf5fc295c0f085602b65c08a0c3cde17e9d0439
Anne Lißner <anne_lissner@gmx.net>
2020-07-25 19:41:23 +0200

    change capitalization for skill level

--- app/models/student.rb
+++ app/models/student.rb
@@ -4,6 +4,6 @@ class Student < ApplicationRecord
   has_many :songs, through: :student_songs
   validates :first_name, presence: true
   validates :last_name, presence: true
-  validates :skill_level, inclusion: { in: %w(beginner intermediate advanced),
-    message: "Skill level should be beginner, intermediate or advanced" }
+  validates :skill_level, inclusion: { in: %w(Beginner Intermediate Advanced),
+    message: "should be Beginner, Intermediate or Advanced" }
 end
