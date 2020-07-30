e88427e05d87975c67f8262160587b7d04b62f0a
Anne Lißner <anne_lissner@gmx.net>
2020-07-30 22:03:22 +0200

    add flatpickr

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,6 +7,7 @@ require("@rails/ujs").start()
 require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
+require("flatpickr/dist/themes/airbnb.css")
 //= require jquery
 //= require jquery_ujs
 //= require_tree .
@@ -27,12 +28,16 @@ require("channels")
 
 // External imports
 import "bootstrap";
-
+import flatpickr from "flatpickr";
 // Internal imports, e.g:
 // import { initSelect2 } from '../components/init_select2';
 // import { dragstart_handler, dragover_handler, drop_handler } from '../components/dnd.js';
+import { LessonDateWithFlatpickr } from '../components/flatpickr';
+import { LessonTimeWithFlatpickr } from '../components/flatpickr';
 
 document.addEventListener('turbolinks:load', () => {
   // Call your functions here, e.g:
   // initSelect2();
+  LessonDateWithFlatpickr();
+  LessonTimeWithFlatpickr();
 });
