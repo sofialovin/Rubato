5de8e4cd7324e903a9432f180cabe3ec3dfc623d
ibrahim shehab <ibrahimshehab@hotmail.com>
2020-07-30 14:44:20 +0200

    installed active storage added an avatar to user

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,6 @@
 class ApplicationController < ActionController::Base
   before_action :authenticate_user!
+  before_action :configure_permitted_parameters, if: :devise_controller?
   include Pundit
 
   after_action :verify_authorized, except: :index, unless: :skip_pundit?
@@ -10,4 +11,11 @@ class ApplicationController < ActionController::Base
   def skip_pundit?
     devise_controller? || params[:controller] =~ /(^(rails_)?admin)|(^pages$)/
   end
+  
+  protected
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
+    devise_parameter_sanitizer.permit(:account_update, keys: [:name, :avatar])
+  end
+  
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,7 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
   has_many :songs
   has_many :students
+  has_one_attached :avatar
   validates :first_name, presence: true
   validates :last_name, presence: true
 end
