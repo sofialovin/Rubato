80f85655ada2ca01bc6a43a664cb46f473a41d47
Anne Lißner <anne_lissner@gmx.net>
2020-08-06 11:15:55 +0200

    add destroy method

--- app/controllers/student_songs_controller.rb
+++ app/controllers/student_songs_controller.rb
@@ -1,10 +1,10 @@
 class StudentSongsController < ApplicationController
 
   def destroy
-    # @student_song = StudentSong.find(params[:test_id])
-    # puts @student_song
-    # @student_song.destroy
-
-    redirect_to student_path
+    @student_song = StudentSong.find(params[:id])
+    @student = Student.find(params[:student_id])
+    @student_song.destroy
+    redirect_to student_path(@student)
+    authorize @student_song
   end
 end
