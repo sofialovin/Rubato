5e59655277c00cbf39c02b44cff45d136ace8d28
Sofia Lovin-Radocea <sofia.lovin1@gmail.com>
2020-08-06 20:43:34 +0300

    updating master

--- db/schema.rb
+++ db/schema.rb
@@ -10,8 +10,8 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
+ActiveRecord::Schema.define(version: 2020_08_06_110548) do
 
-ActiveRecord::Schema.define(version: 2020_08_05_094225) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
