b4ce01a29f15673c3478025c059528fd7afdd75a
Anne Lißner <anne_lissner@gmx.net>
2020-08-13 11:54:06 +0200

    make backdrop appear and disappear correctly

--- app/javascript/components/reload_song_list.js
+++ app/javascript/components/reload_song_list.js
@@ -1,34 +1,95 @@
 const ReloadSongList = () => {
-  // select search form
-  // select div with id exampleModal
-  // on click add class "show"
-  // on click change style to display:block
-  let delay = 5000;
-  let searchForm = document.querySelector(".search-container");
 
-  let songModal = document.getElementById("exampleModal");
+// select all necessary elements
+let searchForm = document.querySelector(".search-container");
+let pageBody = document.querySelector("body");
+let songModal = document.getElementById("exampleModal");
+let div = document.createElement("div");
+let closeButton = document.getElementById("close-button");
+let addSongButton = document.getElementById("add-song-button");
+let closeX = document.getElementById("close-x");
+console.log(songModal.class);
 
-  let pageBody = document.querySelector("body");
+// on submit of form check if query present
 
-  let div = document.createElement("div");
-
-  searchForm.addEventListener('submit', () => {
-    // event.preventDefault();
-    // console.log(songModal);
-    // searchForm.submit();
-    // setTimeout(function() {
-    //   console.log(songModal);
-
-    // }, delay);
-
-    songModal.style.display = "block";
-    pageBody.classList.add("modal-open");
-    document.body.appendChild(div);
-    // div.classList.add("modal-backdrop");
-    // div.classList.add("fade");
-    // div.classList.add("show");
+  window.addEventListener('load', () => {
+    if (QueryPresent() == true) {
+      console.log("Seach query present");
+      openSongModal();
+      document.body.appendChild(div);
+      div.classList.add("modal-backdrop");
+      div.classList.add("fade");
+      div.classList.add("show");
+    } else {
+      console.log("No search query");
+    }
   });
 
+  closeButton.addEventListener('click', () => {
+    console.log("Close button has been clicked");
+    closeSongModal();
+  });
+
+  closeX.addEventListener('click', () => {
+    console.log("X has been clicked");
+    closeSongModal();
+  });
+
+  // pageBody.addEventListener('click', (event) => {
+  //   let backdrop = document.querySelectorAll(".modal-backdrop");
+  //   console.log("Something has been clicked");
+  //   const current = event.currentTarget;
+  //   console.log(current);
+  //   if (!current.id == '#exampleModal' && !currentElement.closest('#exampleModal').length) {
+  //     closeSongModal();
+  //     console.log("Modal has been closed");
+  //   } else {
+  //     // openSongModal();
+  //     console.log("Modal has been opened");
+  //   }
+  // });
+
+  addSongButton.addEventListener('click', () => {
+    console.log('Add Song Button has been clicked');
+    openSongModal();
+  });
+
+  const closeSongModal = () => {
+    let backdrop = document.querySelector(".modal-backdrop");
+    console.log(backdrop);
+    songModal.style.display = "none";
+    songModal.classList.remove("show");
+    pageBody.classList.remove("modal-open");
+    backdrop.remove();
+    console.log("backdrop removed");
+  };
+
+  const openSongModal = () => {
+    let backdrop = document.querySelector(".modal-backdrop");
+    if (document.body.contains(backdrop)) {
+      console.log("backdrop exists");
+      backdrop.remove();
+    } else {
+      console.log("Creating Backdrop and everything");
+      songModal.style.display = "block";
+      songModal.classList.add("show");
+      pageBody.classList.add("modal-open");
+      // document.body.appendChild(div);
+      // div.classList.add("modal-backdrop");
+      // div.classList.add("fade");
+      // div.classList.add("show");
+    }
+  };
+
+};
+
+const QueryPresent = () => {
+  var field = 'query';
+  var url = window.location.href;
+  if(url.indexOf('?' + field + '=') != -1)
+    return true;
+  else
+    return false;
 };
 
 export { ReloadSongList };
