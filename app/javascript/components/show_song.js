dea2eedac0a4b2b59d60353be2d47b5f06c2d288
Anne Lißner <anne_lissner@gmx.net>
2020-08-20 16:13:20 +0200

    fix bug on student page where songs don't show correctly

--- app/javascript/components/show_song.js
+++ app/javascript/components/show_song.js
@@ -3,56 +3,66 @@ const showPageIdentifier = document.querySelector('.show-page-identifier');
 const studentPageIdentifier = document.querySelector('.student-page-identifier');
 
   if (showPageIdentifier || studentPageIdentifier) {
-  const save  =  document.querySelector('#save-area');
-  const title = save.querySelector('#song-title').dataset.title;
-  save.querySelector('#song-title').value = title;
-  save.querySelector('#song-title').style = "font-size:32px;";
-  save.querySelector('#song-title').classList.add("hidden_bg");
-  document.querySelectorAll('input.hide').forEach(input => {
-      input.value = input.dataset.lyrics;
+  // const save  =  document.querySelector('#save-area');
+
+  // Testing Stuff Start
+
+  const saves = document.querySelectorAll('#save-area');
+  saves.forEach((save) => {
+     const title = save.querySelector('#song-title').dataset.title;
+     save.querySelector('#song-title').value = title;
+     save.querySelector('#song-title').style = "font-size:32px;";
+     save.querySelector('#song-title').classList.add("hidden_bg");
+     document.querySelectorAll('input.hide').forEach(input => {
+         input.value = input.dataset.lyrics;
+     });
+
+     document.querySelectorAll('input.lyrics').forEach(input => {
+       input.value = input.dataset.lyrics;
+       input.classList.add("hidden_bg");
+     });
+
+
+       const sel = document.querySelector('.level-select');
+       if (sel) {
+         sel.parentNode.removeChild(sel);
+       }
+
+       // const lbl = document.querySelector('.level-label');
+       // lbl.parentNode.removeChild(lbl);
+
+
+     const btns = document.querySelectorAll(".btn-sm");
+     btns.forEach( (btn) => {
+       btn.parentNode.removeChild(btn);
+     });
+
+    const targs = Array.from(document.querySelectorAll(".target-area"));
+       targs.forEach( (targ) => {
+         targ.classList.add("hidden_bg");
+       });
+
+
+       const drags = Array.from(document.querySelectorAll(".draggable"));
+       drags.forEach( (drag) => {
+         drag.classList.add("hidden_bg");
+       });
+
+
+       const trashes = Array.from(document.querySelectorAll(".trash"));
+       trashes.forEach( (trash) => {
+         trash.parentNode.removeChild(trash);
+       });
+
+       const stretchers = Array.from(document.querySelectorAll(".stretcher"));
+       stretchers.forEach( (stretcher) => {
+         stretcher.parentNode.removeChild(stretcher);
+       });
   });
 
-  document.querySelectorAll('input.lyrics').forEach(input => {
-    input.value = input.dataset.lyrics;
-    input.classList.add("hidden_bg");
-  });
+  // Testing Stuff End
 
 
-    const sel = document.querySelector('.level-select');
-    if (sel) {
-      sel.parentNode.removeChild(sel);
-    }
-
-    // const lbl = document.querySelector('.level-label');
-    // lbl.parentNode.removeChild(lbl);
-
-
-  const btns = document.querySelectorAll(".btn-sm");
-  btns.forEach( (btn) => {
-    btn.parentNode.removeChild(btn);
-  });
-
- const targs = Array.from(document.querySelectorAll(".target-area"));
-    targs.forEach( (targ) => {
-      targ.classList.add("hidden_bg");
-    });
-
-
-    const drags = Array.from(document.querySelectorAll(".draggable"));
-    drags.forEach( (drag) => {
-      drag.classList.add("hidden_bg");
-    });
-
-
-    const trashes = Array.from(document.querySelectorAll(".trash"));
-    trashes.forEach( (trash) => {
-      trash.parentNode.removeChild(trash);
-    });
-
-    const stretchers = Array.from(document.querySelectorAll(".stretcher"));
-    stretchers.forEach( (stretcher) => {
-      stretcher.parentNode.removeChild(stretcher);
-    });
   };
 };
 
