e6166586447f8ad89b5102a975c7de00f476aaba
Anne Lißner <anne_lissner@gmx.net>
2020-08-21 16:45:02 +0200

    cleaning up song/lesson logic

--- app/javascript/controllers/student_page_controller.js
+++ app/javascript/controllers/student_page_controller.js
@@ -1,44 +1,73 @@
 import { Controller } from "stimulus"
 
 export default class extends Controller {
-  static targets = [ "songs", "lessons", "lessonsContent", "songsContent", "allLessonsRightSide", "SongRightSide", "songsList", "songListRightSide"]
+  static targets = [ "songs", "lessons", "lessonsContent", "songsContent", "allLessonsRightSide", "SongRightSide", "songsList", "songListRightSide", "AddSong"]
 
   connect() {
-    //console.log(this.element);
-    //console.log(this.songsTarget);
     if (this.hasLessonsTarget) {
-      this.lessonsTarget.classList.add("selected-item");
-      this.songsTarget.classList.remove("selected-item");
-      this.songsContentTarget.style.display = "none";
-      this.lessonsContentTarget.style.display = "";
-      this.SongRightSideTarget.style.display = "none";
-      this.allLessonsRightSideTarget.style.display = "";
+      if (localStorage.getItem("SongAdded")) {
+        //console.log("Song added is true");
+
+        this.songsTarget.classList.add("selected-item");
+        this.lessonsTarget.classList.remove("selected-item");
+
+        if (this.hasSongsListTarget) {
+          let counter = 0;
+          Array.from(this.songsListTarget.children).forEach((song) => {
+            if (song.classList.contains("active")) {
+              counter += 1;
+            }
+          });
+
+          if (counter == 0 ) {
+            this.songsListTarget.lastElementChild.classList.add("active");
+            this.songListRightSideTarget.lastElementChild.classList.add("active");
+          };
+        }
+
+        this.lessonsContentTarget.style.display = "none";
+        this.songsContentTarget.style.display = "";
+        this.allLessonsRightSideTarget.style.display = "none";
+        this.SongRightSideTarget.style.display = "";
+
+        localStorage.clear();
+
+      } else {
+        this.lessonsTarget.classList.add("selected-item");
+        this.songsTarget.classList.remove("selected-item");
+        this.songsContentTarget.style.display = "none";
+        this.lessonsContentTarget.style.display = "";
+        this.SongRightSideTarget.style.display = "none";
+        this.allLessonsRightSideTarget.style.display = "";
+      }
     }
-    // if (this.hasSongsListTarget) {
-    //   this.songsListTarget.firstElementChild.classList.add("active");
-    //   this.songListRightSideTarget.firstElementChild.classList.add("active");
-    // }
   }
 
   selectSongs() {
-      // console.log("Songs was clicked");
-      this.songsTarget.classList.add("selected-item");
-      this.lessonsTarget.classList.remove("selected-item");
-      console.log(this.songsListTarget);
-      Array.from(this.songsListTarget.children).forEach((song) => {
-         console.log("something");
-       });
-      const counter = 0;
-      // this.songsListTarget.children[0].forEach(child => {
-      //     console.log("something");
-      // })
+    // console.log("Select Songs reached");
+    // console.log("Songs was clicked");
+    this.songsTarget.classList.add("selected-item");
+    this.lessonsTarget.classList.remove("selected-item");
+    // console.log(this.songsListTarget);
 
-      // this.songsListTarget.firstElementChild.classList.add("active");
-      // this.songListRightSideTarget.firstElementChild.classList.add("active");
-      this.lessonsContentTarget.style.display = "none";
-      this.songsContentTarget.style.display = "";
-      this.allLessonsRightSideTarget.style.display = "none";
-      this.SongRightSideTarget.style.display = "";
+    if (this.hasSongsListTarget) {
+      let counter = 0;
+      Array.from(this.songsListTarget.children).forEach((song) => {
+        if (song.classList.contains("active")) {
+          counter += 1;
+        }
+      });
+
+      if (counter == 0 ) {
+        this.songsListTarget.firstElementChild.classList.add("active");
+        this.songListRightSideTarget.firstElementChild.classList.add("active");
+      };
+    }
+
+    this.lessonsContentTarget.style.display = "none";
+    this.songsContentTarget.style.display = "";
+    this.allLessonsRightSideTarget.style.display = "none";
+    this.SongRightSideTarget.style.display = "";
   }
 
   selectLessons() {
@@ -53,4 +82,10 @@ export default class extends Controller {
     }
   }
 
+  addSongAndGoBackToSongs() {
+    // console.log("Song added");
+    this.songsTarget.classList.add("selected-item");
+    this.lessonsTarget.classList.remove("selected-item");
+    localStorage.setItem("SongAdded", true);
+  }
 }
